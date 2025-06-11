7b390ffba8661aeb538550ac58769daede646441
souson7 <souson7@users.noreply.github.com>
2025-06-11 23:29:34 +0900

    フォロー・フォロワー機能におけるルーティング、コントローラ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,15 @@ class User < ApplicationRecord
   # ユーザーをフォローする
   def follow(user_id)
     follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
 end
